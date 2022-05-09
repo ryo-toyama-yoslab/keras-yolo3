1ba8690fe0b4fe5122aab8e2a2a9cef99e98fefe
ryo-toyama-yoslab <99251064+ryo-toyama-yoslab@users.noreply.github.com>
2022-05-09 16:58:22 +0900

    不要コードを削除

--- keras-yolo3_v1/yolo_video.py
+++ keras-yolo3_v1/yolo_video.py
@@ -6,27 +6,6 @@ import glob
 from yolo import YOLO, detect_video
 from PIL import Image
 
-def detect_img(yolo):
-    while True:
-        img = input('Input image filename:')
-        try:
-            image = Image.open(img)
-            img_name = img.replace(".jpg","")
-            img_path = './mAP-master/input/detection-results/' + img_name + ".txt"
-            f = open(img_path, 'a')
-            f.write("")
-            f.close()
-        except:
-            print('Open Error! Try again!')
-            continue
-        else:
-            print("認識実行しますー")
-            r_image = yolo.detect_image2(image, img_path)
-            r_image.show()
-    yolo.close_session()
-
-FLAGS = None
-
 def detect_file(yolo):
     while True:
         files = glob.glob('.\\img_file\\*.jpg')
@@ -124,8 +103,6 @@ if __name__ == '__main__':
         detect_img(YOLO(**vars(FLAGS)))
     elif "file" in FLAGS:
         detect_file(YOLO(**vars(FLAGS)))
-    elif "input" in FLAGS:
-        detect_video(YOLO(**vars(FLAGS)), FLAGS.input, FLAGS.output)
     else:
         print("Must specify at least video_input_path.  See usage with --help.")
     
